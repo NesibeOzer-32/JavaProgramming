7f324d4e2d091ccbbbc036d24413da331915124f
Nesibe Ozer <kosucunesibe@gmail.com>
2022-08-05 13:11:33 +0200

    day39_Recap

---
+++ src/day39_Recap/MethodOverloading.java
@@ -0,0 +1,18 @@
+package day39_Recap;
+
+public class MethodOverloading {
+    private void method1(){
+
+    }
+    private int method1(int a){
+        return a;
+
+    }
+}
+/*
+performed within class
+parameter must be different
+Access specifier can be changed
+private and final methods can be overloaded
+
+ */
