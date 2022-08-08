8b6764f1000b2874d0356a5e01df0660f8a4ca4d
Nesibe Ozer <kosucunesibe@gmail.com>
2022-08-09 01:30:42 +0200

    day40_FinalKeyword

---
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -0,0 +1,20 @@
+package day40_FinalKeyword;
+
+public class ProtectedAccessModifier {
+
+    static String name1="Cydeo";//default access modifier
+    protected static String name2="Wooden Spoon";//protected
+
+    static void method1(){
+
+    }
+
+    protected void method2(){
+
+    }
+
+}
+/*
+Always visible within the same package
+Visible outside the package in subclass ONLY
+ */
