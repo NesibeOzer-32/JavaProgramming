339af1cbbb0b62ec07edd2dba076ff2443a29075
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-11 18:13:08 +0200

    day49-Collection

--- src/day49_Collection/SetInterfacePractice.java
+++ src/day49_Collection/SetInterfacePractice.java
@@ -29,5 +29,34 @@ public class SetInterfacePractice {
         System.out.println("hashSet = " + hashSet);
         //System.out.println(hashSet.get); get methodu yok.
 
+        //except: null. doesnt exept duplicates -Eklendiği sıra ile.
+        // output:linkedHashSet = [10, 200, 300, 40, 90, null]
+        Set<Integer> linkedHashSet= new LinkedHashSet<>();
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(10,200,300,40,90));
+        linkedHashSet.addAll(Arrays.asList(null,null,null));
+
+        System.out.println("linkedHashSet = " + linkedHashSet);
+
+        //doesnt exept: null, sort-sıralar
+        //output:treeSet = [10, 40, 90, 200, 300]
+        Set<Integer> treeSet= new TreeSet<>();
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        treeSet.addAll(Arrays.asList(10,200,300,40,90));
+        //treeSet.addAll(Arrays.asList(null,null,null));
+
+        System.out.println("treeSet = " + treeSet);
+
+        //NullPointerException
+        //String str= null;
+        //System.out.println(str.charAt(0));
+        
+
     }
 }
