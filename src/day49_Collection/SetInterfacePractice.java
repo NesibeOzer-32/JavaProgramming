cee40b46fb221301c36edebf83c21b79eabd8db5
Nesibe Ozer <kosucunesibe@gmail.com>
2022-09-11 17:49:01 +0200

    day39-Collection

---
+++ src/day49_Collection/SetInterfacePractice.java
@@ -0,0 +1,33 @@
+package day49_Collection;
+
+import java.util.*;
+
+public class SetInterfacePractice {
+    public static void main(String[] args) {
+
+        //All of them in the list --output:list = [10, 200, 300, 40, 90, 10, 200,
+        // 300, 40, 90, 10, 200, 300, 40, 90, 10, 200,
+        // 300, 40, 90, 10, 200, 300, 40, 90]
+        List<Integer>list= new ArrayList<>();
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        list.addAll(Arrays.asList(10,200,300,40,90));
+        System.out.println("list = " + list);
+        System.out.println(list.get(4));
+
+        //It's not duplicated. Each number one time. output:hashSet = [200, 40, 10, 90, 300]
+        Set<Integer> hashSet= new HashSet<>();//(random)
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(10,200,300,40,90));
+        hashSet.addAll(Arrays.asList(null,null,null));
+
+        System.out.println("hashSet = " + hashSet);
+        //System.out.println(hashSet.get); get methodu yok
+
+    }
+}
