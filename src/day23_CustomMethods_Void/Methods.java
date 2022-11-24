ccca0412e8ab5a331352c269f7ad1b1f444f6630
Nesibe Ozer <kosucunesibe@gmail.com>
2022-11-24 16:43:07 +0100

    java programming

---
+++ src/day23_CustomMethods_Void/Methods.java
@@ -0,0 +1,13 @@
+package day23_CustomMethods_Void;
+
+public class Methods {
+    //It's a function
+    //Grouping a series of code fragments to perform a task
+    //Allows us to reuse the function rather than repating same set of statements.
+
+    //Declaring a method
+    //Access Modifier   Static Modifier   Return Type   Method Name
+    //    public            static          void       displayMessage(){}
+
+
+}
